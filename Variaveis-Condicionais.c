a6cf6de30dd7d6e221f9e3acc5826c52daff8122
HikariCorallium <maria.sevscuec@gmail.com>
2025-08-04 09:28:49 -0300

    Criado um Arquivo com exemplos de Variaveis e Condicionais

---
+++ Variaveis-Condicionais.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int x = 20;
+    int y =18;
+   if (x > y) {
+       printf("20 é maior que 18");
+   }
+   //--------------//----------------//--------------//---------------
+    printf("\n");
+    int horario = 20;
+    if(horario < 18){
+        printf("\n Bom dia, piazada!");
+    } else{
+        printf("Vai Dormir, Rapaz!");
+    }
+    
+    return 0;
+}
